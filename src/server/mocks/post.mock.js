72c73e07d927337af4bc181bc914b7509de61057
Emmanuel Rodríguez Ramírez <rorem.94.developer@gmail.com>
2022-04-03 18:30:26 -0500

    feat: ✨ added miragejs as minimal server

---
+++ src/server/mocks/post.mock.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        image: 'https://images.unsplash.com/photo-1570654844562-3661dd9ab169?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+        title: 'Nordic Mountains',
+        description:
+            "Some quick example to build on the card title and make up the bulk the card's content.",
+        comments: 98,
+        likes: 100,
+    },
+    {
+        id: 2,
+        image: 'https://images.unsplash.com/photo-1604537529300-3744886c512c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2669&q=80',
+        title: 'England Lake',
+        description:
+            "Some quick example to build on the card title and make up the bulk the card's content.",
+        comments: 25,
+        likes: 50,
+    },
+    {
+        id: 3,
+        image: 'https://images.unsplash.com/photo-1591017403321-dc264d1fd6e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+        title: 'Miami Beach',
+        description:
+            "Some quick example to build on the card title and make up the bulk the card's content.",
+        comments: 2,
+        likes: 15,
+    },
+    {
+        id: 4,
+        image: 'https://images.unsplash.com/photo-1570655670276-2fa0872d03e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2668&q=80',
+        title: 'Canada Landscape',
+        description:
+            "Some quick example to build on the card title and make up the bulk the card's content.",
+        comments: 100,
+        likes: 150,
+    },
+]
